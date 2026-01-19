13695970ca299282262846dc9aa4a090fd6046e6
leongroth <93582490+leongroth@users.noreply.github.com>
2026-01-19 09:49:36 +0100

    Add configuration files (babel, jest, typescript, gitignore)

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.(ts|tsx|js|jsx)'],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.test.{ts,tsx}',
+  ],
+  coverageThresholds: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+};
